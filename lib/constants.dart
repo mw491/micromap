250cd6400518dbd206bc9f5e14a60b3cfc6a1990
mw491 <musaiw@outlook.com>
2024-02-08 21:34:25 +0300

    add constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,9 +5,26 @@ const kBlueColour1 = Color(0xFF093C5A);
 const kBlueColour2 = Color(0xFF184D6A);
 const kBlueColour3 = Color(0xFF00617D);
 const kBlueColour4 = Color(0xFF006E8A);
+const kBlueColour5 = Color(0xFF88B0CD);
 const kCyanColour = Color(0xFF00AE9A);
 const kGreenColour = Color(0xFF69BF50);
 const kYellowColour = Color(0xFFD7DE59);
 const kBeigeColour = Color(0xFFFAF4AA);
 const kOrangeColour = Color(0xFFFDC17C);
 const kBackgroundColour = Color(0xFFE1F1E0);
+
+const kSampleTypes = ['Wound swab', 'Throat swab', 'Skin swab'];
+const kSampleSites = [
+  'Right leg',
+  'Left leg',
+  'Right arm',
+  'Left arm',
+  'Ear canal',
+  'Skin'
+];
+const kMicroOrganisms = [
+  'Escherichia coli',
+  'Staphylococcus aureus',
+  'Clostridium difficile',
+  'Streptococcus pyogenes',
+];
